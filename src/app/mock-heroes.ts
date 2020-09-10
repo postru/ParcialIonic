206e698dcfea8bb46c420f1831e021e75f7c2da2
postru <stiven.postru@gmail.com>
2020-09-10 14:44:18 -0500

    se modifico el mock y listas

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 1, name: 'Fiebre' },
-  { id: 2, name: ' Dolor de garganta' },
+ // { id: 1, name: 'Fiebre' },
+  //{ id: 2, name: ' Dolor de garganta' },
   { id: 3, name: 'Congestion nasal' },
-  { id: 4, name: 'Tos' },
+  //{ id: 4, name: 'Tos' },
   { id: 5, name: 'Dificultad para respirar' },
   { id: 6, name: 'Fatiga' },
   { id: 7, name: 'DEscalofrio' },
-  { id: 8, name: 'Dolor muscular' },
-  { id: 9, name: 'Ninguno de los anteriores' },
+  //{ id: 8, name: 'Dolor muscular' },
+  //{ id: 9, name: 'Ninguno de los anteriores' },
  ];
